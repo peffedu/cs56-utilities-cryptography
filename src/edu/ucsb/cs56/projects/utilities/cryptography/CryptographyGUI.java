ddc0b55eb866b9747b9e93caba6e72891f661129
Ryan Peffers <ryanpeffers@umail.ucsb.edu>
2015-03-13 14:26:17 -0700

    Implemented a Bifid Cipher Button and tested to make sure it works

--- src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
+++ src/edu/ucsb/cs56/projects/utilities/cryptography/CryptographyGUI.java
@@ -14,7 +14,8 @@ import javax.swing.JOptionPane;
    A class to implement the Cryptography GUI.
    @author Callum Steele
    @author Miranda Aperghis
-   @version Project CS56, S13, 06/05/2013
+   @author Ryan Peffers
+   @version Project CS56, W15, 3/10/2015
 */
 
 public class CryptographyGUI
@@ -23,13 +24,15 @@ public class CryptographyGUI
     ShiftCipher shiftCipher = null;
     AffineCipher affineCipher = null;
     VigenereCipher vigenereCipher = null;
+    BifidCipher bifidCipher = null;
+
     String plainText = null;
     String cipherText = null;
     int keyA, keyB;
     String key;
 
     JFrame frame;
-    JButton shift, vigenere, affine, mode;
+    JButton shift, vigenere, affine, bifid, mode;
     JPanel buttonPanel, cipherButtonPanel, modeButtonPanel, textFieldPanel, inputTextPanel, inputKeyTextPanel;
     JTextField input, keyInput;
     JLabel output, inputText, inputKeyText;
@@ -40,178 +43,205 @@ public class CryptographyGUI
 	@param args Default arguments sent to main.
      */
     public static void main (String[] args) {
-	CryptographyGUI cryptoGUI = new CryptographyGUI();
+	    CryptographyGUI cryptoGUI = new CryptographyGUI();
 
-	cryptoGUI.go();
+	    cryptoGUI.go();
     }
 
     /** Function that populates and creates the GUI.
      */
     public void go () {
-	// initialise Cipher objects
-	shiftCipher = new ShiftCipher();
-	affineCipher = new AffineCipher();
-	vigenereCipher = new VigenereCipher();
+        // initialise Cipher objects
+        shiftCipher = new ShiftCipher();
+        affineCipher = new AffineCipher();
+        vigenereCipher = new VigenereCipher();
+        bifidCipher = new BifidCipher();
 
-	// setup overall frame options
-	frame = new JFrame();
-	frame.setSize(400,180);
-	frame.setTitle("Cryptography Interface");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setResizable(false);
+        // setup overall frame options
+        frame = new JFrame();
+        frame.setSize(520,180);
+        frame.setTitle("Cryptography Interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
 
-	// create panel for text fields
-	textFieldPanel = new JPanel();
-	textFieldPanel.setLayout(new BoxLayout(textFieldPanel, BoxLayout.Y_AXIS));
+        // create panel for text fields
+        textFieldPanel = new JPanel();
+        textFieldPanel.setLayout(new BoxLayout(textFieldPanel, BoxLayout.Y_AXIS));
 
-	// create panel containing label and textfield for plaintext input
-	inputTextPanel = new JPanel();
-	inputTextPanel.setLayout(new BoxLayout(inputTextPanel, BoxLayout.X_AXIS));
-	inputText = new JLabel();
-	inputText.setText("Plaintext: ");
-	input = new JTextField();
-	inputTextPanel.add(inputText);
-	inputTextPanel.add(input);
+        // create panel containing label and textfield for plaintext input
+        inputTextPanel = new JPanel();
+        inputTextPanel.setLayout(new BoxLayout(inputTextPanel, BoxLayout.X_AXIS));
+        inputText = new JLabel();
+        inputText.setText("Plaintext: ");
+        input = new JTextField();
+        inputTextPanel.add(inputText);
+        inputTextPanel.add(input);
 
-	// create panel containing label and textfield for key input
-	inputKeyTextPanel = new JPanel();
-	inputKeyTextPanel.setLayout(new BoxLayout(inputKeyTextPanel, BoxLayout.X_AXIS));
-	inputKeyText = new JLabel();
-	inputKeyText.setText("Key(s): ");
-	keyInput = new JTextField();
-	inputKeyTextPanel.add(inputKeyText);
-	inputKeyTextPanel.add(keyInput);
+        // create panel containing label and textfield for key input
+        inputKeyTextPanel = new JPanel();
+        inputKeyTextPanel.setLayout(new BoxLayout(inputKeyTextPanel, BoxLayout.X_AXIS));
+        inputKeyText = new JLabel();
+        inputKeyText.setText("Key(s): ");
+        keyInput = new JTextField();
+        inputKeyTextPanel.add(inputKeyText);
+        inputKeyTextPanel.add(keyInput);
 
-	// add panels to overall text field panel
-	textFieldPanel.add(inputTextPanel);
-	textFieldPanel.add(inputKeyTextPanel);
+        // add panels to overall text field panel
+        textFieldPanel.add(inputTextPanel);
+        textFieldPanel.add(inputKeyTextPanel);
 
-	// create panel for cipher buttons
-	cipherButtonPanel = new JPanel();
-	cipherButtonPanel.setLayout(new BoxLayout(cipherButtonPanel, BoxLayout.X_AXIS));
+        // create panel for cipher buttons
+        cipherButtonPanel = new JPanel();
+        cipherButtonPanel.setLayout(new BoxLayout(cipherButtonPanel, BoxLayout.X_AXIS));
 
-	// create cipher buttons and add listeners
+        // create cipher buttons and add listeners
         shift = new JButton("Shift Cipher");
-	shift.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    // get text from plaintext text field
-		    plainText = input.getText();
+        shift.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // get text from plaintext text field
+                plainText = input.getText();
 
-		    // get key from key text field and set it as key in cipher object
-		    try {
-			keyA = Integer.parseInt(keyInput.getText());
-			shiftCipher.setCipherKey(keyA);
-		    
-			// checks if encrypting or decrypting
-			if (encryptMode)
-			    cipherText = shiftCipher.encrypt(plainText);
-			else
-			    cipherText = shiftCipher.decrypt(plainText);
-			
-			// puts result in the output label
-			output.setText(cipherText);
-		    } catch (Exception ex) {
-			// create popup
-			messagePopUp("Incorrect input for Shift Cipher. Refer to README for correct input format.", "Shift Cipher Input Error");
-		    }
-		}
-	    });
-	affine = new JButton("Affine Cipher");
-	affine.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    // get text from plaintext text field
-		    plainText = input.getText();
+                // get key from key text field and set it as key in cipher object
+                try {
+                keyA = Integer.parseInt(keyInput.getText());
+                shiftCipher.setCipherKey(keyA);
 
-		    // gets keys from key text field and sets as the keys in cipher object
-		    try {
-			key = keyInput.getText();
-			keyA = Integer.parseInt(key.substring(0, key.indexOf(' ')));
-			keyB = Integer.parseInt(key.substring(key.indexOf(' ') + 1));
-			affineCipher.setKeyA(keyA);
-			affineCipher.setKeyB(keyB);
-		    
-			// checks if encrypting or decrypting
-			if (encryptMode)
-			    cipherText = affineCipher.encrypt(plainText);
-			else 
-			    cipherText = affineCipher.decrypt(plainText);
-		    
-			// puts result in the output label
-			output.setText(cipherText);
-		    } catch (Exception ex) {
-			// create popup
-			messagePopUp("Incorrect input for Affine Cipher. Refer to README for correct input format.", "Affine Cipher Input Error");
-		    }
-		}
-	    });
-	vigenere = new JButton("Vigenere Cipher");
-	vigenere.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    // get text from plaintext text field
-		    plainText = input.getText();
+                // checks if encrypting or decrypting
+                if (encryptMode)
+                    cipherText = shiftCipher.encrypt(plainText);
+                else
+                    cipherText = shiftCipher.decrypt(plainText);
 
-		    // gets key from key text field and sets as the key in cipher object
-		    try {
-			key = keyInput.getText();
-			vigenereCipher.setCipherKey(key);
-			
-			// checks if encrypting or decrypting
-			if (encryptMode)
-			    cipherText = vigenereCipher.encrypt(plainText);
-			else
-			    cipherText = vigenereCipher.decrypt(plainText);
-			
-			// puts result in the output label
-			output.setText(cipherText);
-		    } catch (Exception ex) {
-			// create popup
-			messagePopUp("Incorrect input for Vigenere Cipher. Refer to README for correct input format.", "Vigenere Cipher Input Error");
-		    }
-		}
-	    });
+                // puts result in the output label
+                output.setText(cipherText);
+                } catch (Exception ex) {
+                // create popup
+                messagePopUp("Incorrect input for Shift Cipher. Refer to README for correct input format.", "Shift Cipher Input Error");
+                }
+            }
+            });
+        affine = new JButton("Affine Cipher");
+        affine.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // get text from plaintext text field
+                plainText = input.getText();
 
-	// adds cipher buttons to overall cipher button panel
-	cipherButtonPanel.add(shift);
-	cipherButtonPanel.add(affine);
-	cipherButtonPanel.add(vigenere);
+                // gets keys from key text field and sets as the keys in cipher object
+                try {
+                key = keyInput.getText();
+                keyA = Integer.parseInt(key.substring(0, key.indexOf(' ')));
+                keyB = Integer.parseInt(key.substring(key.indexOf(' ') + 1));
+                affineCipher.setKeyA(keyA);
+                affineCipher.setKeyB(keyB);
 
-	// creates panel for mode button
-	modeButtonPanel = new JPanel();
+                // checks if encrypting or decrypting
+                if (encryptMode)
+                    cipherText = affineCipher.encrypt(plainText);
+                else
+                    cipherText = affineCipher.decrypt(plainText);
 
-	// create mode button and adds listener
-	mode = new JButton("Switch to decryption");
-	mode.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    // checks if encryption or decryption mode
-		    if (encryptMode) {
-			encryptMode = false;
-			mode.setText("Switch to encryption");
-		    } else {
-			encryptMode = true;
-			mode.setText("Switch to decryption");
-		    }
-		}
-	    });
+                // puts result in the output label
+                output.setText(cipherText);
+                } catch (Exception ex) {
+                // create popup
+                messagePopUp("Incorrect input for Affine Cipher. Refer to README for correct input format.", "Affine Cipher Input Error");
+                }
+            }
+            });
+        vigenere = new JButton("Vigenere Cipher");
+        vigenere.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // get text from plaintext text field
+                plainText = input.getText();
 
-	// adds mode button to mode panel
-	modeButtonPanel.add(mode);
+                // gets key from key text field and sets as the key in cipher object
+                try {
+                key = keyInput.getText();
+                vigenereCipher.setCipherKey(key);
 
-	// creates overall button panel and adds cipher/mode button panels
-	buttonPanel = new JPanel();
-	buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-	buttonPanel.add(cipherButtonPanel);
-	buttonPanel.add(modeButtonPanel);
+                // checks if encrypting or decrypting
+                if (encryptMode)
+                    cipherText = vigenereCipher.encrypt(plainText);
+                else
+                    cipherText = vigenereCipher.decrypt(plainText);
 
-	// creates output label
-	output = new JLabel();
+                // puts result in the output label
+                output.setText(cipherText);
+                } catch (Exception ex) {
+                // create popup
+                messagePopUp("Incorrect input for Vigenere Cipher. Refer to README for correct input format.", "Vigenere Cipher Input Error");
+                }
+            }
+            });
+        bifid = new JButton("Bifid Cipher");
+        bifid.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // get text from plaintext text field
+                plainText = input.getText();
 
-	// adds components to overall frame
-	frame.getContentPane().add(BorderLayout.CENTER, buttonPanel);
-	frame.getContentPane().add(BorderLayout.NORTH, textFieldPanel);
-	frame.getContentPane().add(BorderLayout.SOUTH, output);
+                // get key from key text field and set it as key in cipher object
+                try {
+                    key = keyInput.getText();
+                    bifidCipher.setCipherKey(key);
 
-	// sets frame visible
-	frame.setVisible(true);
+                    // checks if encrypting or decrypting
+                    if (encryptMode)
+                        cipherText = bifidCipher.encrypt(plainText);
+                    else
+                        cipherText = bifidCipher.decrypt(plainText);
+
+                    // puts result in the output label
+                    output.setText(cipherText);
+                } catch (Exception ex) {
+                    // create popup
+                    messagePopUp("Incorrect input for Bifid Cipher. Refer to README for correct input format.", "Bifid Cipher Input Error");
+                }
+            }
+        });
+
+        // adds cipher buttons to overall cipher button panel
+        cipherButtonPanel.add(shift);
+        cipherButtonPanel.add(affine);
+        cipherButtonPanel.add(vigenere);
+        cipherButtonPanel.add(bifid);
+
+        // creates panel for mode button
+        modeButtonPanel = new JPanel();
+
+        // create mode button and adds listener
+        mode = new JButton("Switch to decryption");
+        mode.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // checks if encryption or decryption mode
+                if (encryptMode) {
+                encryptMode = false;
+                mode.setText("Switch to encryption");
+                } else {
+                encryptMode = true;
+                mode.setText("Switch to decryption");
+                }
+            }
+            });
+
+        // adds mode button to mode panel
+        modeButtonPanel.add(mode);
+
+        // creates overall button panel and adds cipher/mode button panels
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.add(cipherButtonPanel);
+        buttonPanel.add(modeButtonPanel);
+
+        // creates output label
+        output = new JLabel();
+
+        // adds components to overall frame
+        frame.getContentPane().add(BorderLayout.CENTER, buttonPanel);
+        frame.getContentPane().add(BorderLayout.NORTH, textFieldPanel);
+        frame.getContentPane().add(BorderLayout.SOUTH, output);
+
+        // sets frame visible
+        frame.setVisible(true);
     }
 
     public void messagePopUp(String message, String title) {
